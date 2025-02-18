4216556f6a94660c2e64063ba5df0fbd99697ecd
GutsofHyrule <Dresmanns@gmail.com>
2025-02-18 14:53:39 -0500

    handlesubmit

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App() {
 
       console.log('Form Data Sent to Backend:', formData);
 
-      const response = await axios.post('https://your-backend-url/your-endpoint', formData, {
+      const response = await axios.post('https://ai-schedular-backend.onrender.com/api/intro-to-ai-payment', formData, {
         withCredentials: true, //  <-- Important for sending credentials
         headers: {
           'Content-Type': 'application/json' 
